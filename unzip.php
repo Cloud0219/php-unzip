a1674b74d2d425806b56066bec9ab535543e6ef6
MarkLiuChina <siming.liu.cn@gmail.com>
2015-04-16 21:22:35 +0800

    php解压zip文件

---
+++ unzip.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting('E_ALL');
+$zip = new ZipArchive;
+$path = "test_zip.zip";
+
+if ($zip->open($path ) === TRUE)
+{
+	$result = $zip->extractTo('unzip');
+	$zip->close();
+	if($result) {
+		echo "success";
+	}else{
+		echo "failed, check director permission";
+	}
+	
+}else {
+	echo "failed";
+}
+
+
+?>
